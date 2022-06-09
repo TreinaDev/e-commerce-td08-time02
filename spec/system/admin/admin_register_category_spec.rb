93bd2f54998a160e3c21f5bb75264b7623efa03f
nicolasjesse <nicolasjesseam@gmail.com>
2022-06-08 22:16:23 -0300

    Corrige teste fora do escopo
    Estava causando o problema de não resetar o banco de dados por estar fora do escopo do it

--- spec/system/admin/admin_register_category_spec.rb
+++ spec/system/admin/admin_register_category_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe 'Administrador cria categoria' do
-  Category.create!(name: 'Alimentos')
-
   it 'com sucesso' do
+    Category.create!(name: 'Alimentos')
+
     visit root_path
     click_on 'Criar Categoria'
-    fill_in 'Nome', with: 'Uva'
+    fill_in 'Nome', with: 'Fruta'
     select 'Alimentos', from: 'Categoria'
     click_on 'cadastrar'
     
-    category = Category.where(name: 'Uva')
+    category = Category.where(name: 'Fruta')
     expect(category.length).to eq 1
+    expect(category[0].category.name).to eq 'Alimentos'
+    expect(page).to have_content 'E-Commerce'
   end
 end
