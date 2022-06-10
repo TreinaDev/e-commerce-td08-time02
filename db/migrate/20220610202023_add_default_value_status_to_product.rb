e3e8fefc4163fb9b45900f7eafcc22fed1f969ae
kilderMad <costa.kilder@live.com>
2022-06-10 20:03:02 -0300

    correção do erro do git actions

--- db/migrate/20220610202023_add_default_value_status_to_product.rb
+++ db/migrate/20220610202023_add_default_value_status_to_product.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueStatusToProduct < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :products, :status, default: from: nil, to: 0
+    change_column_default :products, :status, default: 0
   end
 end
