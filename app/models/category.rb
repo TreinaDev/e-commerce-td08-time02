bed017b8abbdfb81ad7b27a8667de85d65dd3870
nicolasjesse <nicolasjesseam@gmail.com>
2022-06-10 14:41:56 -0300

    Adiciona dependent em sub_categorias
    Co-authored-by: JamesToledo <jamesmacena14@gmail.com>

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :category, class_name: 'Category', optional: true
-  has_many :categories, class_name: 'Category'
+  has_many :categories, class_name: 'Category', dependent: :destroy
   belongs_to :admin
 
   enum status: { disabled: 0, active: 1 }
