1585544fa6a8eb0553e830283c03d2a18a0d5f0f
nicolasjesse <nicolasjesseam@gmail.com>
2022-06-10 18:10:57 -0300

    Corrige seleção de categorias e Mostra ausência
    Co-authored-by: James Toledo <jamesmacena14@gmail.com>

--- spec/system/admin/admin_view_categories_spec.rb
+++ spec/system/admin/admin_view_categories_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Administrador vê categorias' do
   it 'com sucesso' do
-    admin = Admin.create!(email: 'joão@mercadores.com.br', password: 'joão1234', status: :approved)
+    admin = create(:admin)
     Category.create!(name: 'Eletronicos', admin: admin)
     Category.create!(name: 'Alimentos', admin: admin)
 
@@ -12,5 +12,15 @@ describe 'Administrador vê categorias' do
 
     expect(page).to have_link 'Eletronicos'
     expect(page).to have_link 'Alimentos'
+  end
+
+  it 'e não há categorias cadastradas' do
+    admin = create(:admin)
+
+    login_as(admin, scope: :admin)
+    visit root_path
+    click_on 'Categorias'
+
+    expect(page).to have_content 'Não há categorias cadastradas'
   end
 end
