1a92511f19ff945eea835c37bdc5c525d565af14
kilderMad <costa.kilder@live.com>
2022-06-10 19:54:11 -0300

    testes unitarios e de request
    Co-authored-by: abomfimx <abomfimx@gmail.com>"

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_10_203202) do
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "brand"
-    t.string "description"
-    t.string "sku"
-    t.decimal "width"
-    t.decimal "height"
-    t.decimal "depth"
-    t.decimal "weight"
-    t.decimal "shipping_price"
-    t.boolean "fragile"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +35,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_203202) do
     t.index ["admin_id"], name: "index_categories_on_admin_id"
     t.index ["category_id"], name: "index_categories_on_category_id"
     t.index ["name", "category_id"], name: "index_categories_on_name_and_category_id", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.string "description"
+    t.string "sku"
+    t.decimal "width"
+    t.decimal "height"
+    t.decimal "depth"
+    t.decimal "weight"
+    t.decimal "shipping_price"
+    t.boolean "fragile"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "categories", "admins"
