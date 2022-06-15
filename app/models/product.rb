eddb7b59aa20f3e980d6ee9726f1e51798a8ef30
Marco Fornaciari <mfornaciari@gmail.com>
2022-06-15 16:40:24 -0300

    Exibe preço do dia em detalhes de produto
    Co-authored-by: bomfimx <bomfimx@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ApplicationRecord
   enum status: { inactive: 0, active: 5 }
 
-  validates :name, :brand, :description, :sku, :width, :height, :depth, :weight, presence: true
+  validates :name, :brand, :description, :sku, :width, :height, :depth, :weight, :shipping_price, presence: true
   validates :sku, uniqueness: true
-  validates :weight, :width, :height, :depth, numericality: { greater_than: 0.0 }
+  validates :weight, :width, :height, :depth, :shipping_price, numericality: { greater_than: 0.0 }
 
   has_many :prices, dependent: :nullify
   accepts_nested_attributes_for :prices
   has_one_attached :manual
   has_many_attached :photos
+
+  def current_price
+    price = prices.find_by(
+      'start_date <= current_date AND end_date >= current_date', current_date: Time.zone.today
+    )
+    price.value
+  end
 end
--- spec/system/visitor/product/visitor_views_product_details_spec.rb
+++ spec/system/visitor/product/visitor_views_product_details_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe 'Visitante vê detalhes de um produto' do
-  it 'a partir da tela inicial' do
+  it 'a partir da tela de produtos' do
+    admin = create :admin, name: 'João'
     product = create(:product, name: 'Monitor 8k', brand: 'LG', description: 'Monitor de alta qualidade',
                                sku: 'MON8K-64792', height: 50, width: 100, depth: 12, weight: 12,
                                shipping_price: 23, fragile: true, status: :inactive)
+    create(:price, product: product, admin: admin, start_date: Time.zone.today,
+                   end_date: 1.week.from_now, value: 50.00)
+    create(:price, product: product, admin: admin, start_date: 1.week.from_now + 1,
+                   end_date: 2.weeks.from_now, value: 100.00)
     product.photos.attach(io: File.open(Rails.root.join('spec/support/files/placeholder-image-1.png')),
                           filename: 'placeholder-image-1.png', content_type: 'image/png')
 
@@ -14,7 +19,6 @@ describe 'Visitante vê detalhes de um produto' do
     expect(page).to have_current_path product_path(Product.last.id)
     expect(page).to have_content('Monitor 8k')
     expect(page).to have_css("img[src*='placeholder-image-1.png']")
-    expect(page).to have_content('Inativo')
     expect(page).to have_content('Marca: LG')
     expect(page).to have_content('Descrição: Monitor de alta qualidade')
     expect(page).to have_content('SKU: MON8K-64792')
@@ -22,5 +26,11 @@ describe 'Visitante vê detalhes de um produto' do
     expect(page).to have_content('Peso: 12,00 kg')
     expect(page).to have_content('Preço do Frete: R$ 23,00')
     expect(page).to have_content('Frágil - Sim')
+    expect(page).to have_content 'Preço: R$ 50,00'
+    expect(page).not_to have_content('Inativo')
+    expect(page).not_to have_content(
+      "Preço para #{I18n.l(Time.zone.today)} - #{I18n.l(1.week.from_now)}: R$ 50,00 - " \
+      "Cadastrado por: #{admin.name}"
+    )
   end
 end
