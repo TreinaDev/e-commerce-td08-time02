5439dc265c66d2062ffbbac1e0ab8c15e14b5f5d
Marco Fornaciari <mfornaciari@gmail.com>
2022-06-15 13:43:31 -0300

    Cria testes de sucesso e factory
    Co-authored-by: abomfimx <abomfimx@users.noreply.github.com>

--- spec/factories/prices.rb
+++ spec/factories/prices.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :price do
     association :admin
     association :product
-    value { '9.99' }
-    start_date { '2022-06-14' }
-    end_date { '2022-06-14' }
+    value { 9.99 }
+    start_date { Time.zone.today }
+    end_date { 1.week.from_now }
   end
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -21,6 +21,14 @@ RSpec.describe Price, type: :model do
     expect(price.errors[:start_date]).to include 'não pode ser maior que a data final'
   end
 
+  it 'true se data inicial for menor que data final' do
+    price = described_class.new(start_date: Time.zone.today, end_date: 1.day.from_now)
+
+    price.valid?
+
+    expect(price.errors.include?(:start_date)).to be false
+  end
+
   it 'false se data inicial for anterior à data atual' do
     price = described_class.new(start_date: 1.day.ago)
 
@@ -29,11 +37,17 @@ RSpec.describe Price, type: :model do
     expect(price.errors[:start_date]).to include 'não pode ser anterior a hoje'
   end
 
+  it 'true se data inicial for igual ou posterior à data atual' do
+    price = described_class.new(start_date: Time.zone.today)
+
+    price.valid?
+
+    expect(price.errors.include?(:start_date)).to be false
+  end
+
   it 'false se intervalo já estiver cadastrado' do
-    admin = create :admin
     product = create :product
-    described_class.create!(admin: admin, product: product, start_date: Time.zone.today,
-                            end_date: 1.week.from_now, value: 30.00)
+    create :price, product: product, start_date: Time.zone.today, end_date: 1.week.from_now
     price = described_class.new(product: product, start_date: 1.day.from_now, end_date: 3.days.from_now)
 
     product.reload
@@ -41,5 +55,15 @@ RSpec.describe Price, type: :model do
 
     expect(price.errors[:start_date]).to include 'não pode estar inclusa em intervalo já cadastrado'
     expect(price.errors[:end_date]).to include 'não pode estar inclusa em intervalo já cadastrado'
+  end
+
+  it 'true se intervalo ainda não estiver cadastrado' do
+    product = create :product
+    create :price, product: product, start_date: Time.zone.today, end_date: 1.week.from_now
+    price = described_class.new(product: product, start_date: 8.days.from_now, end_date: 2.weeks.from_now)
+
+    price.valid?
+
+    expect(price.errors.include?(:start_date)).to be false
   end
 end
