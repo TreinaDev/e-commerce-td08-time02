20d1bc8b230fd9233f45f7574944baa8b32608e4
James Toledo <jamesmacena14@gmail.com>
2022-06-20 11:41:17 -0300

    Refatora product_items_controller

--- app/controllers/product_items_controller.rb
+++ app/controllers/product_items_controller.rb
@@ -1,38 +1,34 @@
 class ProductItemsController < ApplicationController
   before_action :authenticate_client!
+  before_action :set_product_item, only: %i[destroy sum_quantity dec_quantity]
   rescue_from ActiveRecord::ActiveRecordError, with: :return_fail
 
   def create
     @product = Product.find(params[:product_id])
-    client = current_client
 
-    unless IncrementItemService.new(@product, client).edit_quantity
-      ProductItem.create(client: client, product: @product)
+    unless IncrementItemService.new(@product, current_client).edit_quantity
+      ProductItem.create(client: current_client, product: @product)
     end
 
     redirect_to @product, notice: "#{@product.name} #{t('added_to_cart')}"
   end
 
   def destroy
-    product_item = ProductItem.find(params[:id])
-    product_item.destroy
+    @product_item.destroy
 
     redirect_to shopping_cart_path, notice: t('product_removed')
   end
 
   def sum_quantity
-    product_item = ProductItem.find(params[:id])
-    product_item.quantity += 1
-    product_item.save
+    @product_item.quantity += 1
+    @product_item.save
 
     redirect_to shopping_cart_path
   end
 
   def dec_quantity
-    product_item = ProductItem.find(params[:id])
-
-    product_item.quantity -= 1
-    product_item.save
+    @product_item.quantity -= 1
+    @product_item.save
 
     redirect_to shopping_cart_path
   end
@@ -41,5 +37,9 @@ class ProductItemsController < ApplicationController
 
   def return_fail
     redirect_to @product, notice: t('add_to_cart_failed')
+  end
+
+  def set_product_item
+    @product_item = ProductItem.find(params[:id])
   end
 end
