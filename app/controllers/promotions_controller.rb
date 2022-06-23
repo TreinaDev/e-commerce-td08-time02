68371593b004e4484fff85194db0a383a6e75e55
nicolasjesse <nicolasjesseam@gmail.com>
2022-06-23 16:38:13 -0300

    Adiciona formulário de criação de promoção
    adiciona tradução
    
    Co-authored-by: abomfimx <abomfimx@users.noreply.github.com>

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,6 +2,21 @@ class PromotionsController < ApplicationController
   before_action :authenticate_admin!
   before_action :set_promotion, only: %i[show]
 
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    @promotion.admin = current_admin
+    if @promotion.save
+      redirect_to @promotion, notice: t('promotion_created')
+    else
+      flash.now[:notice] = t('promotion_not_created')
+      render :new
+    end
+  end
+
   def index
     @promotions = Promotion.all
   end
@@ -12,5 +27,10 @@ class PromotionsController < ApplicationController
 
   def set_promotion
     @promotion = Promotion.find(params[:id])
+  end
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :discount_percentual, :discount_max,
+                                      :usage_limit, :start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :promotions, only: %i[index show]
+  resources :promotions, only: %i[new create index show]
   resources :categories, only: %i[new create index show]
   resources :products, only: %i[index new create show] do
     post 'activate', on: :member
