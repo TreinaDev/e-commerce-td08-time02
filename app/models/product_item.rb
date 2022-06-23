a6204cf27714e070e68b7b771b2c245eb1d80061
kilderMad <costa.kilder@live.com>
2022-06-23 14:30:27 -0300

    coreeçoes no controller
    Co-authored-by: Patrocínio <muckagr@users.noreply.github.com>

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,4 +1,13 @@
 class ProductItem < ApplicationRecord
   belongs_to :product
   belongs_to :client
+
+
+  def get_product_total_price
+    self.product.current_price*self.quantity
+  end
+
+  def get_product_shipping_price
+    self.product.shipping_price*self.quantity
+  end
 end
