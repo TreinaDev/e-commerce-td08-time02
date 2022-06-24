c9d990f1540647d6115a6499b1c17b749ca462a4
James Toledo <jamesmacena14@gmail.com>
2022-06-24 09:53:45 -0300

    Apaga taxas de conversão passadas
    Apaga taxas de conversão passadas quando recebe uma nova taxa
    
    Co-authored-by: Marco Fornaciari <mfornaciari@users.noreply.github.com>

--- spec/requests/apis/exchange_rate_spec.rb
+++ spec/requests/apis/exchange_rate_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'Exchange Rate API' do
   context 'POST /api/v1/exchange_rates' do
     it 'Bem-sucedido' do
+      create :exchange_rate, value: 2.0
       product = create :product, shipping_price: 50.00
       first_price = create :price, admin: product.category.admin, product: product, value: 100.00,
                                    start_date: Time.zone.today, end_date: 2.days.from_now
@@ -14,6 +15,7 @@ describe 'Exchange Rate API' do
       first_price.reload
       second_price.reload
 
+      expect(ExchangeRate.count).to eq 1
       expect(ExchangeRate.last.value).to eq 1.5
       expect(product.rubies_shipping_price).to eq 50.00 / 1.5
       expect(first_price.rubies_value).to eq 100.00 / 1.5
