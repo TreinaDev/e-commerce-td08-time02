9a2d75c608b5550450661d5e1524b0cc618d6b15
Marco Fornaciari <mfornaciari@gmail.com>
2022-06-25 11:19:35 -0300

    Refatora carrinho de compras Extrai alguns métodos para model de cliente e simplifica-os
    Co-authored-by: James Toledo <jamesmacena14@gmail.com>

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,30 +2,10 @@ class ShoppingCartController < ApplicationController
   before_action :authenticate_client!
 
   def index
-    @product_items = ProductItem.where(client_id: current_client.id)
+    @product_items = current_client.product_items
     @item_ids = @product_items.map(&:id)
-    @total = get_total(current_client)
-    @total_shipping = get_total_shipping(current_client)
-    @total_products = get_total_product(current_client)
-  end
-
-  def get_total(current_client)
-    (get_total_product(current_client) + get_total_shipping(current_client)).to_f
-  end
-
-  def get_total_product(current_client)
-    total_value = 0
-    current_client.product_items.each do |product_item|
-      total_value += product_item.define_product_total_price
-    end
-    total_value.to_f
-  end
-
-  def get_total_shipping(current_client)
-    total_value = 0
-    current_client.product_items.each do |product_item|
-      total_value += product_item.define_product_shipping_price
-    end
-    total_value.to_f
+    @total_shipping = current_client.purchase_shipping_value
+    @total_products = current_client.purchase_value
+    @total = @total_shipping + @total_products
   end
 end
--- spec/system/client/purchase/client_confirms_purchase_spec.rb
+++ spec/system/client/purchase/client_confirms_purchase_spec.rb
@@ -2,22 +2,19 @@ require 'rails_helper'
 
 describe 'Cliente confirma compra' do
   it 'com sucesso' do
+    create :exchange_rate, value: 2.0
     client = create :client, code: '510.309.910-14'
-    first_product = create :product, shipping_price: 15.00
-    create :price, admin: first_product.category.admin, product: first_product, value: 50.00
+    first_product = create :product, shipping_price: 10.00
+    create :price, product: first_product, value: 20.00
     second_product = create :product, category: first_product.category, shipping_price: 20.00
-    create :price, admin: second_product.category.admin, product: second_product, value: 35.89
+    create :price, product: second_product, value: 10.00
     create :product_item, client: client, product: first_product, quantity: 1
     create :product_item, client: client, product: second_product, quantity: 1
-    # client_data = { client_wallet: { registered_number: client.code } }
-    # balance_data = { client_wallet: { balance: 120.00, bonus_balance: 0.89 } }.to_json
-    # fake_balance = instance_double Faraday::Response, status: :ok, body: balance_data
-    # allow(Faraday).to receive(:get).with(
-    #   'http://localhost:4000/api/v1/client_wallet/balance', client_data
-    # ).and_return(fake_balance)
-    purchase_status_data = { transaction: { status: 'accepted' } }.to_json
+    purchase_data_sent = { transaction: { order: 1, registered_number: '510.309.910-14',
+                                          value: 3000, cashback: 0 } }.to_json
+    purchase_status_data = { transaction: { order: 1, registered_number: '510.309.910-14',
+                                            status: 'accepted', message: nil } }.to_json
     purchase_response = instance_double Faraday::Response, status: :created, body: purchase_status_data
-    purchase_data_sent = { transaction: { registered_number: client.code, value: '120.89' } }.to_json
     allow(Faraday).to receive(:post).with('http://localhost:4000/api/v1/transactions', purchase_data_sent,
                                           content_type: 'application/json').and_return(purchase_response)
 
